779d8183fdbe38fb2fa34638500f65c822437df2
TienND <tiennd77@gmail.com>
2025-02-04 10:47:33 +0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import BlockViewer from '@/components/BlockViewer.vue';
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import '@/assets/styles.scss';
+import '@/assets/tailwind.css';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.component('BlockViewer', BlockViewer);
+
+app.mount('#app');
